7c6a33e8975b8baf16120412a979bdc9dfdf3e5c
Janne <joriss@utu.fi>
2024-04-03 15:48:08 +0300

    buttons added

--- capstone.py
+++ capstone.py
@@ -9,6 +9,13 @@ def start_btn():
         if btn_rect.collidepoint(event.pos):
             game_state = "draw_game"
             
+            
+def red_btn():
+    global red_rect, pen_color
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if red_rect.collidepoint(event.pos):
+            pen_color = 2
+
 def drawCircleB( screen, x, y ):
     pygame.draw.circle( screen, 'Black', ( x, y ), 5 )
     
@@ -46,16 +53,56 @@ def draw_game():
     global background_color, predict_rect, eraser_rect
     screen.fill((background_color))
     font4 = pygame.font.Font("freesansbold.ttf", 16)
+    
+    #Button for predict
     predict_surface = pygame.Surface((100, 50))
     predict_text = font4.render('Predict', False, 'White')
     text1_rect = predict_text.get_rect(center=(predict_surface.get_width()/2, predict_surface.get_height()/2))
     predict_surface.blit(predict_text, text1_rect)
     screen.blit(predict_surface, (predict_rect.x, predict_rect.y))
+    
+    #Button for switching to eraser
     eraser_surface = pygame.Surface((100, 50))
     eraser_text = font4.render('Eraser', False, 'White')
     text2_rect = eraser_text.get_rect(center=(eraser_surface.get_width()/2, eraser_surface.get_height()/2))
     eraser_surface.blit(eraser_text, text2_rect)
     screen.blit(eraser_surface, (eraser_rect.x, eraser_rect.y))
+    
+    #Button for switching to black pen
+    black_surface = pygame.Surface((100, 50))
+    black_text = font4.render('Black pen', False, 'White')
+    text6_rect = black_text.get_rect(center=(black_surface.get_width()/2, black_surface.get_height()/2))
+    black_surface.blit(black_text, text6_rect)
+    screen.blit(black_surface, (black_rect.x, black_rect.y))
+    
+    #Button for switching to red pen
+    red_surface = pygame.Surface((100, 50))
+    red_text = font4.render('Red pen', False, 'Red')
+    text3_rect = red_text.get_rect(center=(red_surface.get_width()/2, red_surface.get_height()/2))
+    red_surface.blit(red_text, text3_rect)
+    screen.blit(red_surface, (red_rect.x, red_rect.y))
+    
+    #Button for switching to green pen
+    green_surface = pygame.Surface((100, 50))
+    green_text = font4.render('Green pen', False, 'Green')
+    text4_rect = green_text.get_rect(center=(green_surface.get_width()/2, green_surface.get_height()/2))
+    green_surface.blit(green_text, text4_rect)
+    screen.blit(green_surface, (green_rect.x, green_rect.y))
+    
+    #Button for switching to blue pen
+    blue_surface = pygame.Surface((100, 50))
+    blue_text = font4.render('Blue pen', False, 'Blue')
+    text5_rect = blue_text.get_rect(center=(blue_surface.get_width()/2, blue_surface.get_height()/2))
+    blue_surface.blit(blue_text, text5_rect)
+    screen.blit(blue_surface, (blue_rect.x, blue_rect.y))
+    
+    #Button for clearing the whole screen
+    clear_surface = pygame.Surface((100, 50))
+    clear_text = font4.render('Clear screen', False, 'White')
+    text7_rect = clear_text.get_rect(center=(clear_surface.get_width()/2, clear_surface.get_height()/2))
+    clear_surface.blit(clear_text, text7_rect)
+    screen.blit(clear_surface, (clear_rect.x, clear_rect.y))
+    
     
     
     while True: 
@@ -66,7 +113,16 @@ def draw_game():
                 isPressed = False
             elif event.type == pygame.MOUSEMOTION and isPressed == True:         
                 ( x, y ) = pygame.mouse.get_pos()
-                drawCircleB( screen, x, y )
+                if pen_color == 1:
+                    drawCircleB( screen, x, y )
+                elif pen_color == 2:
+                    drawCircleR(screen, x, y)
+                elif pen_color == 3:
+                    drawCircleG(screen, x, y)
+                elif pen_color == 4:
+                    drawCircleBl(screen, x, y)
+                elif pen_color == 0:
+                    drawCircleW(screen, x, y)
             elif event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
@@ -80,9 +136,15 @@ screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Capstone Project')
 background_color = pygame.Color('White')
+pen_color = 1       #colors: 0=eraser 1=black 2=red 3=green 4=blue
 btn_rect = pygame.Rect(600, 600, 100, 100)
 predict_rect = pygame.Rect(0, 300, 100, 100)
 eraser_rect = pygame.Rect(0, 350, 100, 100)
+black_rect = pygame.Rect(0, 400, 100, 100)
+red_rect = pygame.draw.rect(screen, 'Yellow', pygame.Rect(0, 450, 100, 100))
+green_rect = pygame.Rect(0, 500, 100, 100)
+blue_rect = pygame.Rect(0, 550, 100, 100)
+clear_rect = pygame.Rect(0, 600, 100, 100)
 game_state = "start_menu"
 
 
@@ -98,6 +160,7 @@ while True:
         start_btn()
     if game_state == "draw_game":
         draw_game()
+        red_btn()
         
 
     pygame.display.flip()
